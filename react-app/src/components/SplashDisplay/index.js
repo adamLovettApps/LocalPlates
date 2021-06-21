564a9ca56b521fa568a929e0f9742ac607a8b68f
Chris Regan <chrisreg007@gmail.com>
2021-06-21 16:59:55 -0400

    splash css

--- react-app/src/components/SplashDisplay/index.js
+++ react-app/src/components/SplashDisplay/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import './SplashDisplay.css'
 function SplashDisplay(){
     const handleSubmit = (e) => {
 
@@ -20,10 +20,11 @@ function SplashDisplay(){
     },[date,time])
     return(
         <div className="splash-background">
-            <h1>Find a Seat For a Local Plate!</h1>
+            <h1 className="splash-title">Find a Seat For a Local Plate!</h1>
             <form action = {`/search/:${searchTerm}`} method="get">
                 <div className="booking-params">
                     <input type='date'
+                        className= 'home-form-ele'
                         placeholder='date'
                         onChange={(e) => setDate(e.target.value)}
                         value={date}
@@ -31,6 +32,7 @@ function SplashDisplay(){
                     >
                     </input>
                     <input type='time'
+                        className= 'home-form-ele'
                         step="60"
                         placeholder='time'
                         onChange={(e) => setTime(e.target.value)}
@@ -38,13 +40,13 @@ function SplashDisplay(){
                         required
                     >
                     </input>
-                    <input className="home-page-search"
+                    <input className="home-page-search home-form-ele"
                         placeholder="Search by Restaurant, Cuisine Type, or Location"
                         onChange={(e) => setSearchTerm(e.target.value)}
                         required
                     >
                     </input>
-                    <button className='submit-search' type='submit'>Search</button>
+                    <button className='submit-search home-form-ele' type='submit'>Search</button>
                 </div>
             </form>
         </div>
