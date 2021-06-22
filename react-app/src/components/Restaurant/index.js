239609efaf518c69a7d6c491fc10042ac04bf178
jdaniel01 <professor4cob@outlook.com>
2021-06-22 10:50:32 -0700

    working on restaurant component, store and css

--- react-app/src/components/Restaurant/index.js
+++ react-app/src/components/Restaurant/index.js
@@ -1,17 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {getRestaurant} from "../../store/restaurant";
+import { getRestaurant } from "../../store/restaurant";
 
 const Restaurant = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const {id} = useParams();
+    const { id } = useParams();
 
     const sessionUser = useSelector(state => state.session.user)
     const restaurant = useSelector(state => state.restaurant)
 
     const [currentRestaurant, setRestaurant] = useState();
+    const [restaurantPics, setRestaurantPics] = useState();
 
     useEffect(() => {
         dispatch(getRestaurant(id));
@@ -22,4 +23,39 @@ const Restaurant = () => {
             setRestaurant(restaurant);
         }
     }, [currentRestaurant])
+
+
+    return (
+        <div className="body-wrapper">
+            <div className="gallery-slider">
+
+            </div>
+            <div className="title-card">
+                <h1>{currentRestaurant.name}</h1>
+            </div>
+            <div className="reservation-wrapper">
+                <div className="reservation-card">
+                    <input className="party_size" />
+                    <button>Reserve Now</button>
+                </div>
+                <div className="phone-details">
+                    <p>To order delivery or takeout call: {currentRestaurant.phone_number}</p>
+                </div>
+            </div>
+            <div className="review-wrapper">
+                <div className="section-title">
+                    <h3>Reviews</h3>
+                    <button>Submit Review</button>
+                </div>
+                {reviews && reviews.map(review => (
+                    <div key={review.id} className="review-card">
+                        <div className="review-header">
+                            <h4>{review.title}</h4>
+                            
+                        </div>
+                    </div>
+                    ))}
+            </div>
+        </div>
+    )
 }
