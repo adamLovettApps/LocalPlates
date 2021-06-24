fc6bfcd6b12d98a1fa0afdb0bf6065def9038f7c
Adam Lovett <adamlovett@alumni.usc.edu>
2021-06-23 17:43:16 -0700

    add distance sort to front page

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -3,12 +3,20 @@ import SplashDisplay from "../SplashDisplay"
 import CardScroll from "../CardScroll"
 import { useDispatch, useSelector } from "react-redux";
 import {getRestaurants} from "../../store/restaurant"
+
+const getIPInfo = async () => {
+        let res = await fetch('https://ipapi.co/json/');
+        let ip = await res.json();
+        return ip;
+  }
+
 function Home(){
     const dispatch = useDispatch();
     const restaurants = useSelector((state)=>Object.values(state.restaurant.restaurants))
     useEffect(() => {
         (async() => {
-          await dispatch(getRestaurants());
+          let ip = await getIPInfo();
+          await dispatch(getRestaurants(ip));
 
         })();
       }, [dispatch]);
