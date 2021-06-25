3cb76fa744b31b67c72d854efe6163fb232ba05b
Adam Lovett <adamlovett@alumni.usc.edu>
2021-06-25 15:01:04 -0700

    finish booking

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,4 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, TimeField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, IntegerField, DateField
 from app.models import Booking
+
+
+class BookingForm(FlaskForm):
+    restaurant_id = IntegerField('restaurant')
+    user_id = IntegerField('user')
+    booking_day = DateField('date')
+    booking_time = StringField('time')
+    party_size = IntegerField('party size')
--- react-app/src/components/Restaurant/index.js
+++ react-app/src/components/Restaurant/index.js
@@ -4,6 +4,7 @@ import { useHistory, useParams } from "react-router-dom";
 import { getOneRestaurant } from "../../store/restaurant";
 import RestaurantHeader from "./RestaurantHeader";
 import RestaurantInfo from "./RestaurantInfo"
+import BookingCard from "./BookingCard";
 import SeachBar from "../SearchBar"
 import "./Restaurant.css"
 
@@ -22,7 +23,14 @@ function Restaurant(){
         <>
         <div className="search-bar-container"><SeachBar></SeachBar></div>
         <RestaurantHeader></RestaurantHeader>
-        <RestaurantInfo></RestaurantInfo>
+        <div className="top-level-container"> 
+            <div classNam="restaurant-card-container"><RestaurantInfo></RestaurantInfo></div>
+            <div className="right-side-container">
+                <div className="booking-card-container"><BookingCard></BookingCard></div>
+                <div className="map-container"></div>
+            </div>
+        </div>
+        
         </>
 
     )
