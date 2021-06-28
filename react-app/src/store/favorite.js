c2b5241f364da123669a9fb93c529da4c2cb86b2
Adam Lovett <adamlovett@alumni.usc.edu>
2021-06-28 01:12:27 -0700

    refix favorites

--- react-app/src/store/favorite.js
+++ react-app/src/store/favorite.js
@@ -1,8 +1,14 @@
 const SET_FAVORITES = "favorite/SET_FAVORITES";
+const SET_FAVORITES_NO_IP = "favorites/SET_FAVORITES_NO_IP"
 const ADD_FAVORITE = "favorite/ADD_FAVORITE";
 
 const setFavorites = (favorites) => ({
     type: SET_FAVORITES,
+    payload: favorites
+})
+
+const setFavoritesNoIp = (favorites) => ({
+    type: SET_FAVORITES_NO_IP,
     payload: favorites
 })
 
@@ -17,18 +23,28 @@ export const getAllFavorites = (id,ip) => async (dispatch) => {
     }
 }
 
-export const setFavorite = (userId, restaurantId, status,ip) => async (dispatch) => {
-    const response = await fetch(`/api/users/setFavorite/${userId}/${restaurantId}/${status}`);
-
+export const getAllFavoritesNoIP = (id) => async(dispatch) => {
+    const response = await fetch(`/api/users/getfavoritesNoIp/${id}`);
     if (response.ok){
-        const res2 = await await fetch(`/api/users/getfavorites/${userId}/${ip}`);
-        let data = res2.json();
-        dispatch(setFavorites(data))
+        const data = await response.json();
+        dispatch(setFavoritesNoIp(data))
         return data;
     }
 }
 
-const initialState = {favorites: []}
+export const setFavorite = (userId, restaurantId, status) => async (dispatch) => {
+    const response = await fetch(`/api/users/setFavorite/${userId}/${restaurantId}/${status}`);
+    dispatch(getAllFavoritesNoIP(userId));
+    // if (response.ok){
+    //     const res2 = await await fetch(`/api/users/getfavorites/${userId}/${ip}`);
+    //     let data = res2.json();
+    //     dispatch(setFavorites(data))
+    //     return data;
+    // }
+    return;
+}
+
+const initialState = {favorites: [], favoritesNoIp: {}}
 
 export default function reducer(state = initialState, action) {
     switch(action.type) {
@@ -38,7 +54,13 @@ export default function reducer(state = initialState, action) {
                 favArr.push(action.payload[key])
             }
             return {
+                ...state, 
                 favorites: favArr
+            }
+        case SET_FAVORITES_NO_IP:
+            return {
+                ...state,
+                favoritesNoIp: action.payload
             }
         default:
             return state;
